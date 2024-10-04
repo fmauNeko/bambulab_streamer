e4c42aa932e1a3fa2a22f22d85a14a749a5fe639
Florian Maunier <fmauneko@dissidence.ovh>
2024-10-04 17:44:33 +0200

    refactor(auth_data): simplify get_auth_data

--- src/auth_data.rs
+++ src/auth_data.rs
@@ -1,11 +1,7 @@
-use bincode::{config, encode_to_vec};
-
 pub fn get_auth_data(access_code: &str) -> Vec<u8> {
-  let config = config::standard()
+  let config = bincode::config::standard()
     .with_little_endian()
     .with_fixed_int_encoding();
-
-  let mut output: Vec<u8> = Vec::new();
 
   let username = "bblp";
   let mut padded_username = [0_u8; 32];
@@ -14,12 +10,14 @@ pub fn get_auth_data(access_code: &str) -> Vec<u8> {
   let mut padded_access_code = [0_u8; 32];
   padded_access_code[..access_code.len()].copy_from_slice(access_code.as_bytes());
 
-  output.append(&mut encode_to_vec(0x40_u32, config).unwrap());
-  output.append(&mut encode_to_vec(0x3000_u32, config).unwrap());
-  output.append(&mut encode_to_vec(0_u32, config).unwrap());
-  output.append(&mut encode_to_vec(0_u32, config).unwrap());
-  output.append(&mut encode_to_vec(padded_username, config).unwrap());
-  output.append(&mut encode_to_vec(padded_access_code, config).unwrap());
+  let input = (
+    0x40_u32,
+    0x3000_u32,
+    0_u32,
+    0_u32,
+    padded_username,
+    padded_access_code,
+  );
 
-  output
+  bincode::encode_to_vec(input, config).unwrap()
 }
